5c0fe3ade8acf05f46f738c7bf3c6d740b2b1c79
Shorif4321 <shorifali4321@gmail.com>
2025-03-20 23:49:56 +0600

    project setup and header done

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,9 +1,14 @@
+import Image from 'next/image';
 import React from 'react';
+import headerImage from '@/assets/The Dragon News.png'
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className='text-center p-2 md:p-0 my-7 '>
+            <Image className='w-4/6 md:w-3/6 block mx-auto ' src={headerImage} alt='news portal' width={600} height={300} />
+            <p>Journalist Without Fear or Favour</p>
+            <p>Thursday, March 20, 2025</p>
+
         </div>
     );
 };
